44d52fc7ee4abf79ce5df63b36ecb2bf02f354ed
Marcos Costa <78827761+marcoscosta-cloud@users.noreply.github.com>
2024-06-27 14:58:54 -0300

    updateconfig

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // Change the URL of the server and the filename of the csv
 const FILENAME = "certificados.csv";
-const SERVER = "https://raw.githubusercontent.com/marcoscosta-cloud/certificados/main/files/"; 
+const SERVER = "https://raw.githubusercontent.com/marcoscosta-cloud/certificados-repo/main/files/"; 
 
 const FULLURL = SERVER.concat(FILENAME);
