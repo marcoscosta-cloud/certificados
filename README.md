16710f9d69554f3dad754e605787ba593fc0e85e
Marcos Costa <mmarcospc@yahoo.com.br>
2024-06-26 23:16:31 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# Certificados
+
+<a href="https://github.com/marcoscosta-cloud/certificados" target="_blank">
+    <img src="./images/visual.png">
+</a>
+
+Repositório com certificados de cursos, eventos e capacitações.
+
+Certificates management using JavaScript on Github Pages with PDF download and keyword search on datatables.
+
+URL: https://github.com/marcoscosta-cloud/certificados
+
+![GitHub followers](https://img.shields.io/github/followers/marcoscosta-cloud?style=social)
+<a href="https://www.linkedin.com/in/marcospcosta/">
+    <img src="https://img.shields.io/badge/-LinkedIn-blue?style=flat-square&logo=Linkedin&logoColor=white&link=https://www.linkedin.com/in/georgemaia/">
+</a>
+
+## USAGE
+
+Change the SERVER constant in the config.js with your full address of the certificates files.
+
+```javascript
+const SERVER = "http://localhost/certificados-repo/files/"; 
+```
+
+Change the FILENAME constant in the config.js with your certificates list in CSV format.
+
+```javascript
+const FILENAME = "certificados.csv"; 
+```
---
+++ js/certificates.js
@@ -0,0 +1,50 @@
+var urlConfig = "config.js";
+
+    $.getScript(urlConfig, function(){
+        $(document).ready(function(){
+
+            function format_link(link) {
+            if (link)
+                return "<a href='" + SERVER + link + "' target='_blank'><img src='https://img.shields.io/badge/PDF-EC1C24?style=for-the-badge&logo=AdobeAcrobatReader&logoColor=white'></a>";
+            else return "";
+            }
+
+            CsvToHtmlTable.init({
+                csv_path: FULLURL,
+                element: "table-container",
+                allow_download: false,
+                csv_options: {
+                    separator: ",",
+                    delimiter: '"'
+                },
+                datatables_options: {
+                    paging: true,
+                    responsive: {
+                        details: {
+                            display: $.fn.dataTable.Responsive.display.childRow,
+                            type: "column",
+                            target: 'tr'
+                        }
+                    },
+                    colReorder: true,
+                    order: [ 4, 'desc' ],
+                    columnDefs: [
+                        {
+                            className: 'control',
+                            orderable: false,
+                            targets:   0
+                        },
+                        { responsivePriority: 3, targets: 0 },
+                        { responsivePriority: 3, targets: 1 },
+                        { responsivePriority: 5, targets: 2 },
+                        { responsivePriority: 6, targets: 3 },
+                        { responsivePriority: 2, targets: 4 },
+                        { responsivePriority: 4, targets: 5 }
+                    ]
+                },
+                custom_formatting: [
+                    [4, format_link]
+                ]
+            });
+        });
+    });
